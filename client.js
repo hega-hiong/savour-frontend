18df233370d9c9bb9a71c5c877a3c0c27db24233
hega-hiong <hiongkevin7@gmail.com>
2026-04-06 18:32:08 +0100

    deploy frontend

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // URL de ton backend Render
-const API_URL = "https://savour-backend-uhk1.onrender.com";
+const API_URL = "https://savour-backend-1.onrender.com";
 
 // Connexion Socket.io vers Render
 const socket = io(API_URL);
